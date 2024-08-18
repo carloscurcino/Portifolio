21b3774b18001a9393949211a6d22b7e546fb86c
carloscurcino <carlosdanielf541@gmail.com>
2024-08-17 22:44:08 -0300

    chore: setting up the shadcnui library

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 const plugin = require('tailwindcss/plugin')
 module.exports = {
+  darkMode: ["class"],
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
+  prefix: "",
   theme: {
     colors: {
       black: '#000',
@@ -14,32 +19,59 @@ module.exports = {
       'cyan-400': '#058EFF',
       'cyan-300': '#67acc5',
       'cyan-200': '#81D8F7',
-      
+      'slate-100': "#f1f5f9",
+      dark: '#292d34',
+      primary: '#121214',
+      secondary: '#058EFF',
+      light_blue: '#81D8F7',
+      light_red: '#FB143E',
+
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif',
         mono: 'JetBrains Mono, monospace',
-      }
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [
-    plugin(function ({ addUtilities }) {
-      addUtilities({
-        '.scrollbar-hide': {
-          /* IE and Edge */
-          '-ms-overflow-style': 'none',
+  plugins: [require("tailwindcss-animate"),
+  plugin(function ({ addUtilities }) {
+    addUtilities({
+      '.scrollbar-hide': {
+        /* IE and Edge */
+        '-ms-overflow-style': 'none',
 
-          /* Firefox */
-          'scrollbar-width': 'none',
+        /* Firefox */
+        'scrollbar-width': 'none',
 
-          /* Safari and Chrome */
-          '&::-webkit-scrollbar': {
-            display: 'none'
-          }
-        },
-      }
-      )
-    })
+        /* Safari and Chrome */
+        '&::-webkit-scrollbar': {
+          display: 'none'
+        }
+      },
+    }
+    )
+  })
   ],
 }
