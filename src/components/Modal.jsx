f3d7978aa94222acaf172246a5a82719d20691d7
carloscurcino <carlosdanielf541@gmail.com>
2024-08-21 21:39:53 -0300

    Feat: styling the modal to show mobile images and adding a image carousel

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,50 +1,82 @@
 import { X } from "phosphor-react"
 import { useTranslation } from "react-i18next"
+import { Card, CardContent } from "@/components/ui/card"
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
 
 
-export const Modal = ({ setOpenModal, item }) => {
+export const Modal = ({ setOpenModal, item, isMobile }) => {
     const { t } = useTranslation()
     return (
         // Modal Background
         <div className="w-full h-screen bg-[rgba(49,49,49,0.3)] fixed flex justify-center items-center z-20 top-0 left-0 right-0 bottom-0 text-black">
             {/* Modal Container */}
-            <div className="w-[700px] h-[650px] rounded-[12px] bg-white shadow-md flex flex-col ">
-                <div className="flex justify-end">
+            <div className={`w-[700px] h-[650px] rounded-[12px] bg-white shadow-md flex ${isMobile ? 'flex-row-reverse' : 'flex-col'}`}>
+                <div className="z-30 flex justify-end">
                     <X size={25} weight="bold" className="text-white bg-[crimson] hover:bg-[#b91133]/80 p-1 rounded-lg mr-2 cursor-pointer fixed -ml-1 mt-1" onClick={() => { setOpenModal(false) }} />
                 </div>
                 {/* Tittle */}
-                <div
-                    style={{ backgroundImage: `url(${item.altImage})` }}
-                    className="container 
-                    flex flex-col justify-center text-center rounded-t-lg mx-auto bg-no-repeat bg-cover bg-center h-[100%] overflow-hidden"
-                ></div>
-                <div className="inline-block text-center mt-2 text-[22px] font-mono font-bold">
-                    <h1>{item.name}</h1>
 
-                </div>
-                {/* Body */}
-                <div className="flex flex-col flex-[50%] justify-center items-center text-justify text-md px-12">
-                    <p>{t(item.description)}</p>
-                </div>
-                {/* Footer */}
-                <div className="flex flex-[50%] justify-center items-center mt-0">
-                    <a href={item.live} target="_blank">
-                        <button
-                            className="text-center rounded-lg px-4 py-3 m-2
+                {/* <img
+                    src={item.altImage}
+                    className=" 
+                    flex text-center rounded-t-lg object-cover object-no-repeat bg-cover object-center h-[100%] overflow-hidden"
+                /> */}
+                {!isMobile ? (
+                    <div
+                        style={{ backgroundImage: `url(${item.altImage})` }}
+                        className={`container 
+                    flex flex-col justify-center text-center ${isMobile ? 'rounded-r-lg' : 'rounded-t-lg'} mx-auto bg-no-repeat bg-cover bg-center h-[100%] overflow-hidden`}
+                    ></div>
+                )
+                    : (<Carousel className={`relative z-0 flex text-center max-w-[50%] h-[100%]`}>
+                        <CarouselContent className="min-h-full">
+                            {Array.from({ length: 5 }).map((_, index) => (
+                                <CarouselItem key={index} className="min-h-full">
+                                    <img
+                                        src={item.altImage}
+                                        className=" 
+                    flex text-center rounded-r-lg object-cover object-no-repeat bg-cover object-center h-[100%] overflow-hidden"
+                                    />
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                        <CarouselPrevious />
+                        <CarouselNext />
+                    </Carousel>)}
+                <div className={`flex h-full flex-col ${isMobile && 'max-w-[50%] mt-8'}`}>
+                    <div className="inline-block text-center mt-2 text-[22px] font-mono font-bold">
+                        <h1>{item.name}</h1>
+                    </div>
+                    {/* Body */}
+                    <div className="flex flex-col flex-[50%] justify-center items-center text-justify text-md px-12">
+                        <p>{t(item.description)}</p>
+                    </div>
+                    {/* Footer */}
+                    <div className="flex flex-[50%] justify-center items-center mt-0">
+                        <a href={item.live} target="_blank">
+                            <button
+                                className="text-center rounded-lg px-4 py-3 m-2
                             bg-cyan-800 text-white font-bold text-lg"
-                        >
-                            Demo
-                        </button>
-                    </a>
-                    {/* eslint-disable-next-line */}
-                    <a href={item.github} target="_blank">
-                        <button
-                            className="text-center rounded-lg px-4 py-3 m-2
+                            >
+                                Demo
+                            </button>
+                        </a>
+                        {/* eslint-disable-next-line */}
+                        <a href={item.github} target="_blank">
+                            <button
+                                className="text-center rounded-lg px-4 py-3 m-2
                             bg-cyan-800 text-white font-bold text-lg"
-                        >
-                            Code
-                        </button>
-                    </a>
+                            >
+                                Code
+                            </button>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
